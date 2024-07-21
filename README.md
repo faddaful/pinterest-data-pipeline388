caa96c145b45fe5e989e4f3bca2aaf432adece7d
fadda <olayanjubiodun@gmail.com>
2024-07-21 12:15:58 +0100

    Modified readme.md, edit user_posting_emulation.py to reflect the aws configurations and added requirements.txt file.

--- README.md
+++ README.md
@@ -1,1 +1,151 @@
-# pinterest-data-pipeline388
+# Pinterest Data Pipeline
+
+## Table of Contents
+1. [Introduction](#introduction)
+2. [Setting Up Environment](#setting-up-environment)
+    1. [AWS Account](#aws-account)
+    2. [Configure EC2 Instance](#configure-ec2-instance)
+    3. [Setup Kafka on EC2 Instance](#setup-kafka-on-ec2-instance)
+    4. [Create Kafka Topics](#create-kafka-topics)
+3. [Usage Instructions](#usage-instructions)
+4. [File Structure](#file-structure)
+5. [License](#license)
+
+## Introduction
+
+Welcome to the Pinterest Data Pipeline project! This project is designed to extract, process, and analyze Pinterest data efficiently. The main aim of this project is to build a scalable and reliable data pipeline using modern data engineering tools and techniques.
+
+Through this project, I have learned how to:
+- Set up and configure AWS services
+- Deploy and manage Kafka on EC2 instances
+- Create and manage Kafka topics for efficient data streaming
+
+## Setting Up Environment
+
+### AWS Account
+
+To begin, you will need an AWS account. If you don't have one, you can sign up for free at [AWS Free Tier](https://aws.amazon.com/free/). AWS provides various services that we will use to set up and run our data pipeline.
+
+### Configure EC2 Instance
+
+1. **Launch an EC2 Instance**: 
+   - Log in to your AWS Management Console.
+   - Navigate to the EC2 Dashboard.
+   - Click on "Launch Instance".
+   - Select an appropriate Amazon Machine Image (AMI).
+   - Choose an instance type (e.g., t2.micro for testing purposes).
+   - Configure instance details, add storage, and configure security groups.
+   - Review and launch the instance.
+
+2. **Access Your EC2 Instance**:
+   - Use SSH to connect to your EC2 instance.
+   - Ensure you have the necessary permissions set up for your security groups.
+    - create a .pem key file locally in your project folder
+    - Connect to your EC2 instance
+
+### Setup Kafka on EC2 Instance
+
+1. **Install Java**: Kafka requires Java to run.
+   ```bash
+   sudo apt update
+   sudo apt install default-jdk -y
+   ```
+
+2. **Download Kafka**:
+   ```bash
+   wget https://downloads.apache.org/kafka/2.8.0/kafka_2.12-2.8.0.tgz
+   tar -xzf kafka_2.12-2.8.0.tgz
+   cd kafka_2.12-2.8.0
+   cd kafka_2.12-2.8.0/libs
+   ```
+3. **Install AWS MSK**:
+With Amazon MSK, you don't need to worry about managing your kafka operational overhead. MSK automatically provisions and runs your Apache Kafka clusters, as well as monitors the cluster's health.
+    '''
+    bash
+    wget https://github.com/aws/aws-msk-iam-auth/releases/download/v1.1.5/aws-msk-iam-auth-1.1.5-all.jar
+    check that the msk .jar file is in your libs folder
+
+    Add the .jar file 
+    '''
+
+
+3. **Start Kafka Server**:
+   - Start the ZooKeeper service:
+     ```bash
+     bin/zookeeper-server-start.sh config/zookeeper.properties
+     ```
+   - Start the Kafka broker service:
+     ```bash
+     bin/kafka-server-start.sh config/server.properties
+     ```
+
+### Create Kafka Topics
+
+To create Kafka topics, you can use the following command:
+```bash
+bin/kafka-topics.sh --create --topic <topic_name> --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1
+```
+Replace `<topic_name>` with the name of your topic.
+
+### Connect the MSK cluster to a S3 bucket
+You will need to use msk connect to connect the msk cluster to a s3 bucket such that any data going through the cluster will be automaticaaly saved and stored in a dedicated s3 bucket.
+- First download the kafka connector .jar or zip file into your ec2 client from https://www.confluent.io/hub/confluentinc/kafka-connect-s3
+- Upload the connector to your s3 bucket. use the command aws s3 cp kafka-connect-s3 s3://<bucket-name>/kafka-connect-s3/
+ - Create a custom plugin with msk connect in the msk connector console. Select the s3 connector zip file as the plugin object.
+       '''
+    assume admin user privileges
+    <sudo -u ec2-user -i>
+    create directory where we will save our connector 
+    <mkdir kafka-connect-s3 && cd kafka-connect-s3>
+    # download connector from Confluent
+    <wget https://d1i4a15mxbxib1.cloudfront.net/api/plugins/confluentinc/kafka-connect-s3/versions/10.0.3/     confluentinc-kafka-connect-s3-10.0.3.zip>
+    # copy connector to our S3 bucket
+    <aws s3 cp ./confluentinc-kafka-connect-s3-10.0.3.zip s3://<BUCKET_NAME>/kafka-connect-s3/>
+        '''
+ - In your msk console, create a connetor and configure the connector properties as appropriate, select your access role and that's it.
+ - Now, you're ready to start sending data to s3
+
+## We will need to build our own API 
+To replicate the pinterest data pipeline, we need to build our own API. This API will be the be able to send data to the msk cluster which will then be stored in the s3 bucket using the connector.
+1. **Build Kafka REST PROXY integration method for the API**:
+    - Create an API of your desired name
+    - Create a resource on the the API so you can build a proxy integration on it.
+    - On the resources, create a HTTP ANY method, create a resource and use your ec2 publicDNS as your endpoint url. Deploy the API make note of the invoke url.
+2. **Setup Kafka REST PROXY on the EC2 client**
+    -  Install the Confluent package for the Kafka REST Proxy on your EC2 client machine.
+        To install the REST proxy package run the following commands on your EC2 instance:
+        '''
+            sudo wget https://packages.confluent.io/archive/7.2/confluent-7.2.0.tar.gz
+            tar -xvzf confluent-7.2.0.tar.gz 
+        '''
+        cd into confluent-7.2.0/etc/kafka-rest and open kafka-properties to configure it.
+        modify the bootstrap.servers and the zookeeper.connect variables in the file with your already generated values
+        Add the aws-msk-iam configuration from this link to the file for authentication https://github.com/aws/aws-msk-iam-auth
+    - Start and deploy the api if you've not done so before.
+    - Start the REST PROXY 
+    cd to confluent-7.2.0/bin
+    run the command <./kafka-rest-start /home/ec2-user/confluent-7.2.0/etc/kafka-rest/kafka-rest.properties>
+    If everything is setup correctly, you should see a info server running and listening for request.
+3. **Send data to the API**:
+
+
+
+
+
+
+## Usage Instructions
+
+(Instructions on how to use the pipeline will be added as the project progresses.)
+
+## File Structure
+
+(A detailed file structure of the project will be added as the project progresses.)
+
+
+## License
+
+This project is licensed under the MIT License. See the [LICENSE](LICENSE) file for more information.
+
+---
+
+Feel free to contribute to this project or raise issues if you encounter any problems. Happy coding!
--- user_posting_emulation.py
+++ user_posting_emulation.py
@@ -1,72 +1,207 @@
-import requests
-from time import sleep
-import random
-from multiprocessing import Process
-import boto3
 import json
-import sqlalchemy
-from sqlalchemy import text
+import random
+from time import sleep
+import requests
 import yaml
+import sqlalchemy
+from sqlalchemy.sql import text
+import boto3
 import pymysql
 
-
-random.seed(100)
-
-
 class AWSDBConnector:
-
+    """
+    A class to handle the connection to an AWS RDS MySQL database using SQLAlchemy.
+    """
     def __init__(self, db_creds):
+        """
+        Initializes the AWSDBConnector with database credentials file.
+        
+        :param db_creds: Path to the YAML file containing database credentials.
+        """
         self.db_creds = db_creds
 
     def read_db_creds(self):
+        """
+        Reads the database credentials from the YAML file.
+        
+        :return: A dictionary containing the database credentials.
+        """
         with open(self.db_creds, 'r') as file:
             creds = yaml.safe_load(file)
         return creds
         
     def create_db_connector(self):
+        """
+        Creates a SQLAlchemy engine using the database credentials.
+        
+        :return: A SQLAlchemy engine object.
+        """
         creds = self.read_db_creds()
-        engine = sqlalchemy.create_engine(f"mysql+pymysql://{creds['USER']}:{creds['PASSWORD']}@{creds['HOST']}:{creds['PORT']}/{creds['DATABASE']}?charset=utf8mb4")
+        engine = sqlalchemy.create_engine(
+            f"mysql+pymysql://{creds['USER']}:{creds['PASSWORD']}@{creds['HOST']}:{creds['PORT']}/{creds['DATABASE']}?charset=utf8mb4"
+        )
         return engine
 
-
+# Initialize the database connector
 new_connector = AWSDBConnector("db_creds.yaml")
 print(new_connector)
 
-
 def run_infinite_post_data_loop():
+    """
+    Runs an infinite loop that periodically fetches random rows from database tables
+    and posts the data to corresponding API endpoints. Also, uploads the data to S3.
+    """
+    # Create the invoke URLs per topic
+    invoke_urls = {
+        "pin": "https://sjpa7o86pd.execute-api.us-east-1.amazonaws.com/prod/topics/0affe012670f.pin",
+        "geo": "https://sjpa7o86pd.execute-api.us-east-1.amazonaws.com/prod/topics/0affe012670f.geo",
+        "user": "https://sjpa7o86pd.execute-api.us-east-1.amazonaws.com/prod/topics/0affe012670f.user"
+    }
+
+    headers = {'Content-Type': 'application/vnd.kafka.json.v2+json'}
+
     while True:
+        # Sleep for a random interval between 0 and 2 seconds
         sleep(random.randrange(0, 2))
         random_row = random.randint(0, 11000)
         engine = new_connector.create_db_connector()
 
         with engine.connect() as connection:
-
+            # Fetch pinterest data
             pin_string = text(f"SELECT * FROM pinterest_data LIMIT {random_row}, 1")
             pin_selected_row = connection.execute(pin_string)
-            
+            pin_result = {}
             for row in pin_selected_row:
                 pin_result = dict(row._mapping)
-
+            
+            # Fetch geolocation data
             geo_string = text(f"SELECT * FROM geolocation_data LIMIT {random_row}, 1")
             geo_selected_row = connection.execute(geo_string)
-            
+            geo_result = {}
             for row in geo_selected_row:
                 geo_result = dict(row._mapping)
-
+            
+            # Fetch user data
             user_string = text(f"SELECT * FROM user_data LIMIT {random_row}, 1")
             user_selected_row = connection.execute(user_string)
-            
+            user_result = {}
             for row in user_selected_row:
                 user_result = dict(row._mapping)
-            
-            print(pin_result)
-            print(geo_result)
-            print(user_result)
 
+            # Send data to the corresponding endpoint
+            try:
+                for topic, result in zip(["pin", "geo", "user"], [pin_result, geo_result, user_result]):
+                    payload = {"records": [{"value": result}]}
+                    response = requests.post(invoke_urls[topic], headers=headers, json=payload)
+                    print(f"Posted to {topic} with response: {response.status_code}")
+                    if response.status_code != 200:
+                        print(f"Error posting to {topic}: {response.text}")
+            except Exception as e:
+                print(f"Exception occurred: {e}")
+
+            # Optionally, store data to S3
+            try:
+                s3_client = boto3.client('s3')
+                s3_client.put_object(
+                    Bucket='user-0affe012670f-bucket',
+                    Key=f'pinterest_data/{random_row}.json',
+                    Body=str(pin_result)
+                )
+                s3_client.put_object(
+                    Bucket='user-0affe012670f-bucket',
+                    Key=f'geolocation_data/{random_row}.json',
+                    Body=str(geo_result)
+                )
+                s3_client.put_object(
+                    Bucket='user-0affe012670f-bucket',
+                    Key=f'user_data/{random_row}.json',
+                    Body=str(user_result)
+                )
+                print("Data uploaded to S3")
+            except Exception as e:
+                print(f"Exception occurred during S3 upload: {e}")
 
 if __name__ == "__main__":
+    # Run the infinite data posting loop
     run_infinite_post_data_loop()
     print('Working')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#random.seed(100)
+
+# class AWSDBConnector:
+
+#     def __init__(self, db_creds):
+#         self.db_creds = db_creds
+
+#     def read_db_creds(self):
+#         with open(self.db_creds, 'r') as file:
+#             creds = yaml.safe_load(file)
+#         return creds
+        
+#     def create_db_connector(self):
+#         creds = self.read_db_creds()
+#         engine = sqlalchemy.create_engine(f"mysql+pymysql://{creds['USER']}:{creds['PASSWORD']}@{creds['HOST']}:{creds['PORT']}/{creds['DATABASE']}?charset=utf8mb4")
+#         return engine
+
+
+# new_connector = AWSDBConnector("db_creds.yaml")
+# print(new_connector)
+
+
+# def run_infinite_post_data_loop():
+#     while True:
+#         sleep(random.randrange(0, 2))
+#         random_row = random.randint(0, 11000)
+#         engine = new_connector.create_db_connector()
+
+#         with engine.connect() as connection:
+
+#             pin_string = text(f"SELECT * FROM pinterest_data LIMIT {random_row}, 1")
+#             pin_selected_row = connection.execute(pin_string)
+            
+#             for row in pin_selected_row:
+#                 pin_result = dict(row._mapping)
+
+#             geo_string = text(f"SELECT * FROM geolocation_data LIMIT {random_row}, 1")
+#             geo_selected_row = connection.execute(geo_string)
+            
+#             for row in geo_selected_row:
+#                 geo_result = dict(row._mapping)
+
+#             user_string = text(f"SELECT * FROM user_data LIMIT {random_row}, 1")
+#             user_selected_row = connection.execute(user_string)
+            
+#             for row in user_selected_row:
+#                 user_result = dict(row._mapping)
+            
+#             print(pin_result)
+#             print(geo_result)
+#             print(user_result)
+
+
+# if __name__ == "__main__":
+#     # Create the invoke url per topic
+#     invoke_url = "https://sjpa7o86pd.execute-api.us-east-1.amazonaws.com/prod/topics/0affe012670f.pin"
+#     invoke_url = "https://sjpa7o86pd.execute-api.us-east-1.amazonaws.com/prod/topics/0affe012670f.geo"
+#     invoke_url = "https://sjpa7o86pd.execute-api.us-east-1.amazonaws.com/prod/topics/0affe012670f.user"
+
+#     headers = {'Content-Type': 'application/vnd.kafka.json.v2+json'}
+#     response = requests.request("POST", invoke_url, headers=headers, data=payload)
+#     run_infinite_post_data_loop()
+#     print('Working')
     
     
 
